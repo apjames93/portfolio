5c4379b4b37fb6031e59eedc486b063f479cb0c8
Alexander James <AlexJames@Alexanders-MacBook-Air.local>
2016-08-25 13:42:55 -0600

    added new project

--- public/components/projects/projects-service.js
+++ public/components/projects/projects-service.js
@@ -10,6 +10,13 @@
       };
       function projectsInfo(){
         var projects = [{
+          title: 'Nightmare',
+          discription: 'This app is a first-person shooter game in which you kill zombies with a watergun.Technologies used angularJS, jwt bcrypt, Node.JS, Express, Knex.js Unity, C# and Blender to make the 3D map',
+          github :  'https://github.com/The-Nightmare/App/blob/development/README.md',
+          Deployed : 'https://nightmare-f6a77.firebaseapp.com',
+          img : './img/Screen Shot 2016-08-24 at 10.35.54 PM.png'
+        },
+        {
           title: 'The Hive',
           discription: 'PostgresQl, node.js, JavaScript, google maps api, Knex.js, passport google oauth. App is designed for bee spotting enthusiast, made to show location of bees seen along with type and image of the bee. PostgresQl, node.js, JavaScript, google maps api, Knex.js, passport google oauth.',
           github :  'https://github.com/TheHiveOrg/The-Hive',
@@ -28,22 +35,15 @@
           discription: 'Walkthrough for making a decoupled angular app with a restful api and postgresql database .',
           github :  'https://github.com/apjames93/brick-breaker-game',
           Deployed : 'https://brick-breaker-game.firebaseapp.com',
-          img : './img/Screen Shot 2016-08-22 at 10.27.44 AM.png'
+          img : './img/Screen Shot 2016-08-11 at 5.41.08 PM.png'
+
         },
         {
           title: 'Car Project',
           discription: 'using jQuery and edmunds api app finds motor and transmission specs for cars.',
           github :  'https://github.com/apjames93/carProject',
           Deployed : 'https://carproject.firebaseio.com',
-          img : './img/Screen Shot 2016-08-11 at 5.41.08 PM.png'
-        },
-        {
-          title: 'Nightmare',
-          discription: 'This app is a first-person shooter game in which you kill zombies with a watergun.Technologies used angularJS, jwt bcrypt, Node.JS, Express, Knex.js Unity, C# and Blender to make the 3D map',
-          github :  'https://github.com/The-Nightmare/App/blob/development/README.md',
-          Deployed : 'https://nightmare-f6a77.firebaseapp.com',
-          img : './img/Screen Shot 2016-08-24 at 10.35.54 PM.png'
-
+          img : './img/Screen Shot 2016-08-22 at 10.27.44 AM.png'
         }];
         return projects;
       }
